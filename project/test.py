57306011d1cbf3beef153a17970ede7a367ee617
llllllllllIIIIl <jiyangli123@gmail.com>
2026-02-12 11:04:48 -0800

    update

--- project/test.py
+++ project/test.py
@@ -2,6 +2,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '''
+    <h1>Index Page</h1>
+    <form method="post" action="/hello">
+        <input type="text" name="go to hello" placeholder="enter something">
+        <button type="submit">Continue</button>
+    </form>
+    '''
+@app.route('/hello', methods=['POST']) 
+def hello():
+    return 'Hello, World'
+
+if __name__ == '__main__':
+    app.run(debug=True)
