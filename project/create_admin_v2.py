c245b7f8bce11954cfdccf6109a67d90120898ed
llllllllllIIIIl <jiyangli123@gmail.com>
2026-02-28 16:25:56 -0800

    Create create_admin_v2.py

---
+++ project/create_admin_v2.py
@@ -0,0 +1,47 @@
+
+from model_v2 import init_db, get_session, Admin
+import hashlib
+
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+engine = init_db('sqlite:///polling_v2.db')
+db = get_session(engine)
+
+
+existing_admin = db.query(Admin).first()
+if existing_admin:
+    print(f"\n Admin exists: {existing_admin.username}")
+    choice = input("Still create new admin? (y/n): ")
+    if choice.lower() != 'y':
+        print("Cancelled. No new admin created.")
+        db.close()
+        exit()
+
+
+print("\nEnter username and password:")
+username = input("Username: ").strip()
+password = input("Password: ").strip()
+
+if not username or not password:
+    print("Error")
+    db.close()
+    exit()
+
+
+if db.query(Admin).filter_by(username=username).first():
+    print(f"'{username}' exists")
+    db.close()
+    exit()
+
+admin = Admin(
+    username=username,
+    password_hash=hash_password(password)
+)
+
+db.add(admin)
+db.commit()
+
+
+
+db.close()
